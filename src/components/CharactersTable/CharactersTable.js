0210f7b0286ba7bee590ed843bac657be66609f9
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2022-05-13 13:04:47 +0530

    Character images added to the table, style changes, improved for mobile view.

--- src/components/CharactersTable/CharactersTable.js
+++ src/components/CharactersTable/CharactersTable.js
@@ -5,6 +5,7 @@ export default function CharacterTable({ character }) {
 	return (
 		<div>
 			<div className={styles.heading}>
+				<div className={styles.heading_flag}></div>
 				<button className={styles.heading_name}>
 					<div>Name</div>
 				</button>
@@ -21,9 +22,12 @@ export default function CharacterTable({ character }) {
 				return (
 					<Link href={`/character/${character.id}`}>
 						<div className={styles.row} key={index}>
-							<div className={styles.name} >{character.name}</div>
-							<div className={styles.species} >{character.species}</div>
-							<div className={styles.gender} >{character.gender}</div>
+							<div className={styles.flags}>
+								<img src={character.image} alt={character.name} />
+							</div>
+							<div className={styles.name}>{character.name}</div>
+							<div className={styles.species}>{character.species}</div>
+							<div className={styles.gender}>{character.gender}</div>
 						</div>
 					</Link>
 				);
